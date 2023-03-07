966ba976b477a5f39f2277519615695a73e5c5f7
AndradeItalo <ifa2@cin.ufpe.br>
2023-03-06 22:15:34 -0300

    SRA, SLLV, SLL, SLT and SLTI feitos

--- created/controlUnit.v
+++ created/controlUnit.v
@@ -31,7 +31,8 @@ parameter ST_RESET = 6'b111111;
 parameter ST_SLL = 6'b000011;
 parameter ST_SLLV = 6'b000111;
 parameter ST_SLT = 6'b001111;
-parameter ST_SLTI = 6'b011111;'
+parameter ST_SLTI = 6'b011111;
+parameter ST_SRA = 6'b000010;
 
 
 parameter TYPE_R = 6'h0;
@@ -41,6 +42,7 @@ parameter OP_SLL = 6'h0;
 parameter OP_SLLV = 6'h4;
 parameter OP_SLT = 6'h2a;
 parameter OP_SLTI = 6'ha;
+parameter OP_SRA = 6'h3;
 
 
     initial begin
@@ -178,6 +180,9 @@ parameter OP_SLTI = 6'ha;
                                     end
                                     OP_SLT: begin
                                         STATE = ST_SLT;
+                                    end
+                                    OP_SRA: begin
+                                        STATE = ST_SRA;
                                     end
                                 endcase
                             end
@@ -439,6 +444,47 @@ parameter OP_SLTI = 6'ha;
                         muxSRInputSel = 2'b00;
                         muxSRControl = 3'b000; 
                         muxSRNumSel = 2'b00;
+
+                        COUNTER = COUNTER + 1;
+                    end
+                end
+                
+                ST_SRA: begin
+                    if (COUNTER == 6'd0) begin
+                        STATE = ST_SRA;
+                        PCWrite = 1'b0;
+                        memRW = 1'b0;
+                        IRWrite = 1'b0;
+                        RegWrite = 1'b0;
+                        aluOP = 3'b000; 
+                        muxIord = 3'b000;
+                        muxAluSrcA = 3'b000; 
+                        muxAluSrcB = 3'b000; 
+                        muxRegDst = 3'b000;
+                        muxMemToReg = 3'b000;
+                        muxPCSource = 3'b000;
+                        muxSRInputSel = 2'b01; ///
+                        muxSRControl = 3'b001; ///
+                        muxSRNumSel = 2'b00;
+
+                        COUNTER = COUNTER + 1;
+                    end
+                    else if (COUNTER == 6'd1) begin
+                        STATE = ST_COMMON;
+                        PCWrite = 1'b0;
+                        memRW = 1'b0;
+                        IRWrite = 1'b0;
+                        RegWrite = 1'b0; 
+                        aluOP = 3'b000; 
+                        muxIord = 3'b000;
+                        muxAluSrcA = 3'b000; 
+                        muxAluSrcB = 3'b000; 
+                        muxRegDst = 3'b000;  
+                        muxMemToReg = 3'b000; 
+                        muxPCSource = 3'b000;
+                        muxSRInputSel = 2'b00;
+                        muxSRControl = 3'b100; ///
+                        muxSRNumSel = 2'b01; ///
 
                         COUNTER = COUNTER + 1;
                     end
